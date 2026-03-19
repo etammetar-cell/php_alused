e5cbf61d2b3a62bee430f45a3b74ed2a760e19a4
etammetar-cell <etammetar@hkhk.edu.ee>
2026-03-19 10:29:07 +0200

    uued asjad

--- single_car.php
+++ single_car.php
@@ -1,5 +1,5 @@
 <?php include('config.php'); ?>
-
+<?php include('header.php'); ?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -9,36 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
-    <!-- /menüü -->
-<nav class="navbar navbar-expand-lg bg-body-tertiary mb-4">
-  <div class="container">
-    <a class="navbar-brand" href="#">Autorent</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-          </ul>
-      <form class="d-flex" role="search" method="get" action="index.php">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="otsi">
-        <button class="btn btn-outline-success" type="submit">Otsi</button>
-      </form>
-    </div>
-  </div>
-</nav>
- <!-- /menüü -->
+    
 <!-- /sisu -->
  <div class="container">
     <a href="index.php" class="btn btn-success">Tagasi</a>
@@ -54,10 +25,14 @@
      ?>
             <div class="col">
                 <h1>Nimi: <?php echo $rida["mark"]; ?><?php echo $rida["model"]; ?></h1>
-                <p>Mootor:<?php echo $rida["engine"]; ?></p>
-                <p>Kütus:<?php echo $rida["fuel"]; ?></p>
-                <p>Hind:<?php echo $rida["price"]; ?>€/päev</p>
-                
+                <p>Mootor: <?php echo $rida["engine"]; ?></p>
+                <p>Kütus: <?php echo $rida["fuel"]; ?></p>
+                <p>Aasta: <?php echo $rida["year"]; ?></p>
+                <p>Staatus: <?php echo $rida["status"]; ?></p>
+                <p>Käigukast: <?php echo $rida["transmission"]; ?></p>
+                <p>Istmed: <?php echo $rida["seats"]; ?></p>
+                <p class="fs-5"><p>Hind: <?php echo $rida["price"]; ?>€/päev</p>
+                <a href="#" class="btn btn-dark w-100">Rendi auto</a>
             </div>
             <div class="col">
                  <img src="https://loremflickr.com/800/500/<?php echo str_replace(" ","", $rida["mark"]); ?>" class="card-img-top" alt="<?php echo str_replace(" ","", $rida["mark"]) ?>">
