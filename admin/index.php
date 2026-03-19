86d333cd3bfced52fe95570e5a0bfbe1fbdf4cd1
etammetar-cell <etammetar@hkhk.edu.ee>
2026-03-19 11:35:39 +0200

    admin

--- admin/index.php
+++ admin/index.php
@@ -27,11 +27,51 @@
 
      //var_dump($_GET["otsi"]);
      $valjund = mysqli_query($yhendus, $paring);  //saadan päringu andmebaasi
-     while($rida = mysqli_fetch_assoc($valjund)){ //sikutan vastuse alla
-     //var_dump($rida);
+     
                 //Kuvan testvastuse
      ?>
-  <div class="col">
+     <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">id</th>
+      <th scope="col">Mudel</th>
+      <th scope="col">Hind</th>
+      <th scope="col">Mootor</th>
+      <th scope="col">Kütus</th>
+      <th scope="col">Mark</th>
+      <th scope="col">Aasta</th>
+      <th scope="col">Staatus</th>
+      <th scope="col">Engine</th>
+      <th scope="col">Engine</th>
+      <th scope="col">Engine</th>
+      <th scope="col">Engine</th>
+    </tr>
+  </thead>
+  <tbody>
+    <?php
+      while($rida = mysqli_fetch_assoc($valjund)){ //sikutan vastuse alla
+     //var_dump($rida);
+    ?>
+    <tr>
+      <th scope="row"><?php echo $rida["id"]; ?></th>
+      <td><?php echo $rida["model"]; ?></td>
+      <td><?php echo $rida["price"]; ?></td>
+      <td><?php echo $rida["engine"]; ?></td>
+      <td><?php echo $rida["fuel"]; ?></td>
+      <td><?php echo $rida["mark"]; ?></td>
+      <td><?php echo $rida["year"]; ?></td>
+      <td><?php echo $rida["status"]; ?></td>
+      <td><?php echo $rida["engine"]; ?></td>
+      <td><?php echo $rida["engine"]; ?></td>
+      <td><?php echo $rida["engine"]; ?></td>
+      <td><?php echo $rida["engine"]; ?></td>
+      
+    </tr>
+   <?php } ?>
+
+  </tbody>
+</table>
+  <!-- <div class="col">
     <div class="card">
       <img src="https://loremflickr.com/400/250/<?php echo str_replace(" ","", $rida["mark"]); ?>" class="card-img-top" alt="<?php echo str_replace(" ","", $rida["mark"]) ?>">
       <div class="card-body">
@@ -44,8 +84,7 @@
         <a href="single_car.php?id=<?php echo $rida["id"]; ?>" class="btn btn-primary w-100">Rendi</a>
       </div>
       </div>
-</div>
-<?php } ?>
+</div> -->
 <!-- /1auto -->
 </div>
 </div>
